4d073deee16f63c8ee5631c0b6db0548c7292ae9
mohamed <alsydshmawym@gmail.com>
2024-11-22 23:31:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace print_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
